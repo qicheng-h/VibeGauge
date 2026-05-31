1acbc1739ba55950ec603ae939465073f538cef9
qicheng-h <qicheng-h@users.noreply.github.com>
2026-05-31 11:04:55 -0700

    Apply app icon

--- build-widget.sh
+++ build-widget.sh
@@ -7,6 +7,7 @@ CONTENTS_DIR="${APP_DIR}/Contents"
 MACOS_DIR="${CONTENTS_DIR}/MacOS"
 RESOURCES_DIR="${CONTENTS_DIR}/Resources"
 EXECUTABLE_NAME="CodexCreditsWidget"
+ICON_FILE="AppIcon.icns"
 
 mkdir -p .build
 mkdir -p .build/module-cache
@@ -20,6 +21,8 @@ swiftc \
   -o "${MACOS_DIR}/${EXECUTABLE_NAME}" \
   -framework AppKit
 
+cp "${ICON_FILE}" "${RESOURCES_DIR}/${ICON_FILE}"
+
 /usr/libexec/PlistBuddy -c "Clear dict" "${CONTENTS_DIR}/Info.plist" 2>/dev/null || true
 /usr/libexec/PlistBuddy -c "Add :CFBundleName string ${APP_NAME}" "${CONTENTS_DIR}/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :CFBundleDisplayName string ${APP_NAME}" "${CONTENTS_DIR}/Info.plist"
@@ -27,6 +30,7 @@ swiftc \
 /usr/libexec/PlistBuddy -c "Add :CFBundleVersion string 1" "${CONTENTS_DIR}/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :CFBundleShortVersionString string 1.0" "${CONTENTS_DIR}/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :CFBundleExecutable string ${EXECUTABLE_NAME}" "${CONTENTS_DIR}/Info.plist"
+/usr/libexec/PlistBuddy -c "Add :CFBundleIconFile string AppIcon" "${CONTENTS_DIR}/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :CFBundlePackageType string APPL" "${CONTENTS_DIR}/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :LSMinimumSystemVersion string 15.0" "${CONTENTS_DIR}/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :LSUIElement bool true" "${CONTENTS_DIR}/Info.plist"
